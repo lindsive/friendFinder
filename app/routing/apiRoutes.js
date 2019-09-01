9fbb8c67d5c40b0e87031ebcf421a3fa6fd36bad
Lindsay Iveson <ivesonlindsay@gmail.com>
2019-09-01 16:23:43 -0400

    added content to home.html

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,2 +1,16 @@
 // Loading Data
-var 
+var friendData = require("../data/friends");
+
+// Routing
+module.exports = function(app) {
+
+    // GET Request
+    app.get("/api/friends", function(req, res) {
+        res.json(friendData);
+    });
+
+    // POST Request
+    app.post("/api/survey", function(req, res) {
+        
+    })
+}
