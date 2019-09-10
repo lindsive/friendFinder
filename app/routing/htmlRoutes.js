7a993f1420415d72ca85819e433ecd1b98d3fa98
Lindsay Iveson <ivesonlindsay@gmail.com>
2019-09-09 20:46:15 -0400

    added Num() and created variables to turn newFriend.answers array into integers, & pushed into a new array

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -4,11 +4,12 @@ var path = require("path");
 // Routing to HTML page content
 module.exports = (app) => {
 
-    app.get("*", function(req, res) {
+    app.get("/", function(req, res) {
         res.sendFile(path.join(__dirname, "../public/home.html"));
     });
 
     app.get("/survey", function(req, res) {
         res.sendFile(path.join(__dirname, "../public/survey.html"));
     });
+    
 };
